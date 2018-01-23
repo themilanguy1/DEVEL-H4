04a1eab3fee83b67ab6eb82cdf8672745979770a
themilanguy1 <milan.gupta@ymail.com>
2018-01-23 18:08:58 +0100

    w

--- opdracht 11.php
+++ opdracht 11.php
@@ -1,13 +1,26 @@
 <?php
-$n = 5;
+// $n = 5;
 
-$x= 0;
-while($x <= $n) {
+// $x= 1;
+// $array1 = array();
+// $end = end($array1);
+// echo "$n! = "; 
+// while($x <= $n) {
+//     array_push($array1,$x);
+//     if($x == $end) {
+//         echo "end =";
+//     }
+//     else {
+//         echo "$x x ";
+//         $x++;
+//     }
+// }
+// echo array_product($array1);
 
+?>
 
-}
+<?php
 
-echo $x;
 
 
 ?>
